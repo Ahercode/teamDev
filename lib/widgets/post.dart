e2281f24b710c0fabddb28d9dc9c81aa0b959a87
ahercode <philipkelly407@gmail.com>
2021-05-19 00:28:18 +0000

    Update post.dart

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+
+import 'package:animator/animator.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fashinapp/models/user.dart';
